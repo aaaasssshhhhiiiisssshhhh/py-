4da84e84f92a6f22946f21809409c772db7c8293
aashishkarki11 <aashishkarki11@gmail.com>
2021-11-19 19:59:49 +0545

    adding two numbers

---
+++ py/add .py
@@ -0,0 +1,16 @@
+#taking direct values 
+
+a = 1 
+b = 1 
+c = a+b
+print ('sum:',c)
+
+
+#taking values from user 
+
+x = int (input("enter first value:"))
+y = int ( input("enter second value:"))
+
+sum = x+y 
+
+print("the sum is :", sum)
