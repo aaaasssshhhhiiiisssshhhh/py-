cdaaeff69c1f68a2045f6e50452b3c29cca4f17a
aashishkarki11 <aashishkarki11@gmail.com>
2021-11-19 20:57:22 +0545

    commited a funny if statement

---
+++ py/if.py
@@ -0,0 +1,18 @@
+#here lets learn about if statement which you might have known before hehe 😂
+# here i will write a funny if statement code.... have a laugh with me 
+
+name = str (input('enter a name:'))
+if name == 'aashish':
+    print ('funny man with sense of humor ')
+    
+elif name == 'willsir':
+    print ('he is a legit programmer')
+
+elif name == 'bibek':
+    print ('chota pack bada dhamaka')
+ 
+elif name == 'shiva':
+    print ('behahari kto mah123')
+
+else:
+    print ('hehe nam aayena ni timro solti')                
