c759bba880454fc21ec4dec2d7059b7dd195dcb5
aashishkarki11 <aashishkarki11@gmail.com>
2021-11-19 20:14:38 +0545

    comparing two numbers

---
+++ py/compare2numbers.py
@@ -0,0 +1,35 @@
+#compare two numbers and finding out the greater one:
+
+a = 5 
+b = 6 
+if a>b: #condition ( where we evaluate if a is greater then b or not)
+    
+    
+    print('the greater value is a:',a)
+
+
+else: #condition ( if b > a this condition work )
+    
+    print ("the greater value is b:", b)    
+    
+
+
+
+
+
+
+
+#likewise we can take values form user and compare two numbers:
+
+x = int (input("enter first value:")) #taking first value from user
+y = int ( input("enter second value:")) # taking second value from user
+
+if x>y: #condition ( where we evaluate if x is greater then y or not)
+    
+    
+    print('the greater value is a:',x)
+
+
+else: #condition ( if y > x this condition work )
+    
+    print ("the greater value is b:", y)   
