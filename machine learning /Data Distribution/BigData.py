8308a862830446ecfc298d70e7576d9a4ce3a701
Aashish Karki <47684436+aaaasssshhhhiiiisssshhhh@users.noreply.github.com>
2021-11-30 22:45:24 +0545

    commit about big data cocept

---
+++ machine learning /Data Distribution/BigData.py
@@ -0,0 +1,8 @@
+import numpy
+import matplotlib.pyplot as aashish
+
+x = numpy.random.uniform (0.0 , 5.0 , 100000)
+aashish.hist(x,10)
+aashish.show()
+
+#its a basic exaple of what histogram.py is 
